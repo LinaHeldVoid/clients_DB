9bd64d4db3e4edbd2c1f579506ac2bbc2c018cf4
Lin Amber <iltep118666@gmail.com>
2023-02-13 22:17:52 +0400

    Осталось прописать выбор для всех функций поиска

--- functions_module.py
+++ functions_module.py
@@ -1,6 +1,6 @@
 import psycopg2
 
-conn = psycopg2.connect(database='...', user='...', password='...')
+conn = psycopg2.connect(database="...", user="...", password="...")
 
 
 # Создание таблиц
@@ -39,15 +39,16 @@ def create_tables():
 # проверка на "нулевой ввод"
 def zero_input_check(input_data):
     if len(input_data) == 0:
-        print('Данная графа обязательна для заполнения! Попробуйте ещё раз.')
+        print("Данная графа обязательна для заполнения! Попробуйте ещё раз.")
         par = 1
     else:
         par = 0
     return par
 
+
 # неизвестная команда
 def bad_input():
-    print('Ваш ответ не распознан. Попробуйте ещё раз.')
+    print("Ваш ответ не распознан. Попробуйте ещё раз.")
 
 
 # расшифрофка данных из БД:
@@ -68,7 +69,7 @@ def change_name(client_id, new_name):
         WHERE id = %s; 
         """, (new_name, client_id))
         conn.commit()
-        print('Имя успешно изменено!')
+        print("Имя успешно изменено!")
         return
 
 
@@ -80,7 +81,7 @@ def change_second_name(client_id, new_data):
         WHERE id = %s; 
         """, (new_data, client_id))
         conn.commit()
-        print('Фамилия успешно изменена!')
+        print("Фамилия успешно изменена!")
         return
 
 
@@ -92,67 +93,99 @@ def change_email(client_id, new_email):
         WHERE client_id = %s; 
         """, (new_email, client_id))
         conn.commit()
-        print('Email успешно изменён!')
+        print("Email успешно изменён!")
         return
 
 
-def change_phone_number(client_id):
+def append_phone_number(client_id):
     client_id = int(client_id)
     print(client_id)
     print(type(client_id))
     phone_list = phone_insert()
     with conn.cursor() as cur:
-        cur.execute("""
-        DELETE FROM phone_number
-        WHERE phone_number.client_id = %s; 
-        """, (client_id, ))
         for numbers in phone_list:
             cur.execute("""
             INSERT INTO phone_number VALUES(%s, %s);
             """, (numbers, client_id))
         conn.commit()
-        print('Телефон успешно изменён!')
-        return
+    print("Номер телефона успешно изменён!")
+    return
 
 
 # реализация множества или ни одного номера телефона
 def phone_insert():
     phone_list = []
     while True:
-        phone_number = input('Номер телефона: ')
+        phone_number = input("Номер телефона: ")
         if zero_input_check(phone_number):
             continue
         phone_list.append(phone_number)
-        choice1 = input('Принято. Желаете ввести другой номер телефона? ').lower()
+        choice1 = input("Принято. Желаете ввести другой номер телефона? ").lower()
         if zero_input_check(choice1):
             continue
-        elif choice1.lower() == 'да':
+        elif choice1.lower() == "да":
             continue
-        elif choice1.lower() == 'нет':
+        elif choice1.lower() == "нет":
             return phone_list
         else:
             bad_input()
             continue
 
 
+# поиск клиента в базе
+def search_data(string, check_string, search_point, table):
+    while True:
+        if zero_input_check(search_point):
+            continue
+        else:
+            with conn.cursor() as cur:
+                cur.execute("""
+                SELECT %s FROM %s
+                WHERE %s = %s;
+                """, (string, table, check_string, search_point))
+                check_list = cur.fetchall()
+                print(check_list)
+                if zero_input_check(check_list):
+                    continue
+                else:
+                    data = translate(check_list)
+                    if data is None:
+                        print("Такой информации в реестре нет! Попробуйте ещё раз.")
+                        continue
+                    else:
+                        return search_point
+
+
+# поиск id по фамилии клиента
+def get_id(data, table, search_point):
+    with conn.cursor() as curr:
+        curr.execute("""
+        SELECT id FROM %s
+        WHERE %s = %s;
+        """, (table.replace('"', ""), data.replace('"', ""), search_point.replace('"', "")))
+        client_id = curr.fetchall()
+        client_id = translate(client_id)
+    return client_id
+
+
 # Добавить нового клиента
 def new_client():
     # чтение данных пользователя
-    print('Добавление нового клиента. Введите следующие данные:')
+    print("Добавление нового клиента. Введите следующие данные:")
     while True:
-        name = input('Имя: ')
+        name = input("Имя: ")
         if zero_input_check(name) == 1:
             continue
         else:
             break
     while True:
-        second_name = input('Фамилия: ')
+        second_name = input("Фамилия: ")
         if zero_input_check(second_name):
             continue
         else:
             break
     while True:
-        email = input('Email: ')
+        email = input("Email: ")
         if zero_input_check(email):
             continue
         else:
@@ -174,15 +207,15 @@ def new_client():
         INSERT INTO email VALUES(%s, %s);
         """, (email, i))
     while True:
-        choice = input('Желаете ввести номер телефона?').lower()
-        if choice.lower() == 'да':
+        choice = input("Желаете ввести номер телефона?").lower()
+        if choice.lower() == "да":
             phone_list = phone_insert()
             with conn.cursor() as curr:
                 for numbers in phone_list:
                     curr.execute("""
                     INSERT INTO phone_number VALUES(%s, %s);
                     """, (numbers, i))
-        elif choice.lower() == 'нет':
+        elif choice.lower() == "нет":
             break
         else:
             bad_input()
@@ -193,98 +226,188 @@ def new_client():
 
 # Добавить номер телефона для существующего клиента
 def add_phone_number():
+    search_point = input("Введите фамилию клиента: ")
+    search_point = search_data("second_name", "second_name", search_point, "clients")
+    phone_list = phone_insert()
+    client_id = get_id("second_name", "clients", search_point)
+    with conn.cursor() as curr:
+        for numbers in phone_list:
+            curr.execute("""
+            INSERT INTO phone_number VALUES(%s, %s);
+            """, (numbers, client_id))
+        conn.commit()
+        response = "Номер телефона добавлен!"
+        print(response)
+        return
+
+
+# изменение данных клиента
+def change_data():
+    search_point = input("Введите фамилию клиента: ")
+    search_point = search_data("second_name", "second_name", search_point, "clients")
+    client_id = get_id("second_name", "clients", search_point)
     while True:
-        search_point = input('Введите фамилию клиента: ')
-        if zero_input_check(search_point):
-            continue
+        data = input("Какую информацию нужно изменить?" + "\n"
+                     "1) Имя" + "\n"
+                     "2) Фамилия" + "\n"
+                     "3) Email" + "\n"
+                     "4) Телефон" + "\n")
+        if data.isnumeric() == 1:
+            data = int(data)
         else:
-            with conn.cursor() as cur:
-                cur.execute("""
-                SELECT second_name FROM clients;
-                """)
-                check_list = cur.fetchall()
-                print(check_list)
-                if zero_input_check(check_list):
+            print("Пожалуйста, введите ответ цифрой")
+            continue
+        if data == 1:
+            while True:
+                new_name = input("Введите новое имя: ")
+                if zero_input_check(new_name):
                     continue
                 else:
-                    data = translate(check_list)
-                    if data is None:
-                        print('Такой фамилии в реестре нет! Попробуйте ещё раз.')
-                        continue
-                    else:
-                        phone_list = phone_insert()
-                        with conn.cursor() as cursor:
-                            cursor.execute("""
-                            SELECT id FROM clients WHERE second_name=%s;
-                            """, search_point)
-                            check_id = cursor.fetchall()
-                            client_id = translate(check_id)
-                        with conn.cursor() as curr:
-                            for numbers in phone_list:
-                                curr.execute("""
-                                INSERT INTO phone_number VALUES(%s, %s);
-                                """, (numbers, client_id))
-                            conn.commit()
-                            response = 'Номер телефона добавлен!'
-                            print(response)
-                            return
+                    change_name(client_id, new_name)
+                    break
+        elif data == 2:
+            while True:
+                new_second_name = input("Введите новую фамилию: ")
+                if zero_input_check(new_second_name):
+                    continue
+                else:
+                    change_second_name(client_id, new_second_name)
+                    break
+        elif data == 3:
+            while True:
+                new_email = input("Введите новый email: ")
+                if zero_input_check(new_email):
+                    continue
+                else:
+                    change_email(client_id, new_email)
+                    return
+        elif data == 4:
+            append_phone_number(client_id)
+            return
+        else:
+            bad_input()
+            continue
 
 
-def change_data():
+# Удалить номер телефона для существующего клиента
+def delete_number():
+    search_point = input("Введите фамилию клиента: ")
+    search_point = search_data("second_name", "second_name", search_point, "clients")
+    client_id = get_id("second_name", "clients", search_point)
+    with conn.cursor() as cur:
+        cur.execute("""
+        DELETE FROM phone_number
+        WHERE client_id = %s; 
+        """, (client_id,))
+        conn.commit()
+    print("Номера телефонов для данного клиента удалены.")
+    return
+
+
+# Удалить клиента из базы
+def delete_client():
+    search_point = input("Введите фамилию клиента: ")
+    search_point = search_data("second_name", "second_name", search_point, "clients")
+    client_id = get_id("second_name", "clients", search_point)
+    with conn.cursor() as cur:
+        cur.execute("""
+        DELETE FROM clients
+        WHERE client_id = %s; 
+        """, (client_id,))
+        cur.execute("""
+        DELETE FROM email
+        WHERE client_id = %s; 
+        """, (client_id,))
+        cur.execute("""
+        DELETE FROM phone_number
+        WHERE client_id = %s; 
+        """, (client_id,))
+        conn.commit()
+    print("Данные клиента удалены!")
+    return
+
+
+# Найти клиента по предоставленной информации
+def show_data():
     while True:
-        search_point = input('Введите фамилию клиента: ')
-        with conn.cursor() as cur:
-            cur.execute("""
-            SELECT second_name FROM clients WHERE second_name=%s;
-            """, search_point)
-            search_point = cur.fetchall()
-            true_input = translate(search_point)
-            if true_input is None:
-                print('Такой фамилии в реестре нет! Попробуйте ещё раз.')
-                continue
-            else:
-                while True:
-                    with conn.cursor() as curr:
-                        curr.execute("""
-                        SELECT id FROM clients
-                        WHERE second_name = %s;
-                        """, search_point)
-                        client_id = curr.fetchall()
-                        client_id = translate(client_id)
-                    data = input('Какую информацию нужно изменить?' + '\n'
-                                 '1) Имя' + '\n'
-                                 '2) Фамилия' + '\n'
-                                 '3) Email' + '\n'
-                                 '4) Телефон' + '\n')
-                    if data.isnumeric() == 1:
-                        data = int(data)
-                    else:
-                        print('Пожалуйста, введите ответ цифрой')
-                        continue
-                    if data == 1:
-                        new_name = input('Введите новое имя: ')
-                        if zero_input_check(new_name):
-                            continue
-                        else:
-                            change_name(client_id, new_name)
-                            return
-                    elif data == 2:
-                        new_second_name = input('Введите новую фамилию: ')
-                        if zero_input_check(new_second_name):
-                            continue
-                        else:
-                            change_second_name(client_id, new_second_name)
-                            return
-                    elif data == 3:
-                        new_email = input('Введите новый email: ')
-                        if zero_input_check(new_email):
-                            continue
-                        else:
-                            change_email(client_id, new_email)
-                            return
-                    elif data == 4:
-                        change_phone_number(client_id)
-                        return
-                    else:
-                        bad_input()
-                        continue
+        data = input("По каким данным нужно найти клиента?" + "\n"
+                     "1) Имя" + "\n"
+                     "2) Фамилия" + "\n"
+                     "3) Email" + "\n"
+                     "4) Телефон" + "\n")
+        if data.isnumeric() == 1:
+            data = int(data)
+        else:
+            print("Пожалуйста, введите ответ цифрой")
+            continue
+        if data == 1:
+            while True:
+                search_point = input("Введите имя: ")
+                if zero_input_check(search_point):
+                    continue
+                else:
+                    client_id = translate(get_id("name", "clients", search_point))
+                    print(f"ID: {client_id}" + "\n")
+                    name = translate(search_data("name", "name", search_point, "clients"))
+                    print(f"Имя: {name}" + "\n")
+                    second_name = translate(search_data("second_name", "name", search_point, "clients"))
+                    print(f"Фамилия: {second_name}" + "\n")
+                    email = translate(search_data("email", "client_id", client_id, "email"))
+                    print(f"Email: {email}" + "\n")
+                    phone_number = translate(search_data("number", "client_id", client_id, "phone_number"))
+                    print(f"Номер телефона: {phone_number}" + "\n")
+                    break
+        elif data == 2:
+            while True:
+                search_point = input("Введите фамилию: ")
+                if zero_input_check(search_point):
+                    continue
+                else:
+                    client_id = translate(get_id("second_name", "clients", search_point))
+                    name = translate(search_data("name", "second_name", search_point, "clients"))
+                    second_name = translate(search_data("second_name", "second_name", search_point, "clients"))
+                    email = translate(search_data("email", "client_id", client_id, "email"))
+                    phone_number = translate(search_data("number", "client_id", client_id, "phone_number"))
+                    print(f"ID: {client_id}" + "\n")
+                    print(f"Имя: {name}" + "\n")
+                    print(f"Фамилия: {second_name}" + "\n")
+                    print(f"Email: {email}" + "\n")
+                    print(f"Номер телефона: {phone_number}" + "\n")
+                    break
+        elif data == 3:
+            while True:
+                search_point = input("Введите email: ")
+                if zero_input_check(search_point):
+                    continue
+                else:
+                    client_id = translate(get_id("email", "email", search_point))
+                    name = translate(search_data("name", "id", client_id, "clients"))
+                    second_name = translate(search_data("second_name", "id", client_id, "clients"))
+                    email = translate(search_data("email", "client_id", client_id, "email"))
+                    phone_number = translate(search_data("number", "client_id", client_id, "phone_number"))
+                    print(f"ID: {client_id}" + "\n")
+                    print(f"Имя: {name}" + "\n")
+                    print(f"Фамилия: {second_name}" + "\n")
+                    print(f"Email: {email}" + "\n")
+                    print(f"Номер телефона: {phone_number}" + "\n")
+                    return
+        elif data == 4:
+            while True:
+                search_point = input("Введите номер телефона: ")
+                if zero_input_check(search_point):
+                    continue
+                else:
+                    client_id = translate(get_id("phone_number", "number", search_point))
+                    name = translate(search_data("name", "id", client_id, "clients"))
+                    second_name = translate(search_data("second_name", "id", client_id, "clients"))
+                    email = translate(search_data("email", "client_id", client_id, "email"))
+                    phone_number = translate(search_data("number", "client_id", client_id, "phone_number"))
+                    print(f"ID: {client_id}" + "\n")
+                    print(f"Имя: {name}" + "\n")
+                    print(f"Фамилия: {second_name}" + "\n")
+                    print(f"Email: {email}" + "\n")
+                    print(f"Номер телефона: {phone_number}" + "\n")
+                    return
+        else:
+            bad_input()
+            continue
--- main_program.py
+++ main_program.py
@@ -7,8 +7,11 @@ def main():
         choice = input('Каким будет вам следующий шаг? ' + '\n'
                        '1) Добавить нового клиента' + '\n'
                        '2) Изменить номер телефона у существующего клиента' + '\n'
-                       '3) Изменить данные о существующем клиенте' + '\n'                                                      
-                       '4) Завершить работу' + '\n')
+                       '3) Изменить данные о существующем клиенте' + '\n'
+                       '4) Удалить номер телефона для существующего клиента' + '\n'
+                       '5) Удалить клиента из базы' + '\n'
+                       '6) Найти клиента по внесённым данным' + '\n'
+                       '7) Завершить работу' + '\n')
         if choice.isnumeric() == 1:
             choice = int(choice)
         else:
@@ -24,6 +27,14 @@ def main():
             functions_module.change_data()
             conn.commit()
         elif choice == 4:
+            functions_module.delete_number()
+            conn.commit()
+        elif choice == 5:
+            functions_module.delete_client()
+            conn.commit()
+        elif choice == 6:
+            functions_module.show_data()
+        elif choice == 7:
             conn.commit()
             conn.close()
             print('Программа завершает свою работу.')
